324a70a1d5d090cab60e69675eb89430cfdb3f90
liubodong <bl576@cornell.edu>
2023-10-18 20:32:27 -0400

    Implement data types stock and portfolio in portfolio module

--- src/portfolio.ml
+++ src/portfolio.ml
@@ -1,38 +1,12 @@
 (* Portfolio.ml - Intended to store portfolio data *)
-type stock = { symbol : string; price : float; quantity : int }
+type stock = {
+  symbol : string;
+  price : float;
+  quantity : int;
+}
 
 type portfolio = {
   balance : float;
-  bank_account : float;
+  bank_account : int;
   followed_stocks : stock list;
 }
-
-let create_portfolio initial_balance initial_bank_account =
-  {
-    balance = initial_balance;
-    bank_account = initial_bank_account;
-    followed_stocks = [];
-  }
-
-let add_stock portfolio symbol price quantity =
-  let stock = { symbol; price; quantity } in
-  { portfolio with followed_stocks = stock :: portfolio.followed_stocks }
-
-let update_balance portfolio amount =
-  if portfolio.balance +. amount >= 0.0 then (
-    print_string
-      ("Balance updated: " ^ string_of_float (portfolio.balance +. amount));
-    { portfolio with balance = portfolio.balance +. amount })
-  else (
-    print_string
-      ("Still need: " ^ string_of_float (0.0 -. portfolio.balance -. amount));
-    portfolio)
-
-let update_bank_account portfolio amount =
-  { portfolio with bank_account = portfolio.bank_account +. amount }
-
-let remove_stock portfolio symbol =
-  let updated_stocks =
-    List.filter (fun stock -> stock.symbol <> symbol) portfolio.followed_stocks
-  in
-  { portfolio with followed_stocks = updated_stocks }
--- src/portfolio.mli
+++ src/portfolio.mli
@@ -1,28 +1,32 @@
 (* Portfolio.mli - Intended to store portfolio data *)
 
-(* This should be from the stockdata module but I'll just leave it here since
-   sotckdata is not implemented yet*)
-type stock = { symbol : string; price : float; quantity : int }
+(* Message to who's making stock.ml: This should be from the stock module but
+   I'll just leave it here since stock is not implemented yet*)
+type stock = {
+  symbol : string;
+  price : float;
+  quantity : int;
+}
 
 type portfolio = {
   balance : float;
-  account : int;
+  bank_account : int;
   followed_stocks : stock list;
 }
 
 val create_portfolio : float -> int -> portfolio
-(** Create a portfolio *)
+(** Create a portfolio with [initial_balance] and [initial_bank_account]*)
 
 val add_stock : portfolio -> stock -> portfolio
-(** Add a stock to the watchlist of the portfolio*)
+(** Add [stock] to the watchlist of the portfolio*)
 
 val update_balance : portfolio -> float -> portfolio
-(** Update balance by [n] amount. Print out a message to indicate the updated
-    balance. If [n] is negative and its absolute value exceeds current 
-    balance, produce a message "Out of balance: [balance + n] needed"*)
+(** Update balance by [amount]. Print out a message to indicate the updated
+    balance. If [amount] is negative and its absolute value exceeds current
+    balance, produce a message "Out of balance: [balance + amount] needed"*)
 
 val update_bant_account : portfolio -> int -> portfolio
 (** Update the current bank account. *)
 
-val remove_stock : portfolio -> stock -> portfolio
-(** Remove a stock from the watchlist. *)
+val remove_stock : portfolio -> string -> portfolio
+(** Remove a stock from the watchlist. Required: the stock is in the watchlist. *)
