a04f4b07373a1ab0548e25476d6510a7954d9c99
liubodong <bl576@cornell.edu>
2023-10-18 21:08:47 -0400

    Create placeholders for functions in src/portfolio.ml 10/18/23 - bl576@cornell.edu - created placeholders for functions

--- src/portfolio.ml
+++ src/portfolio.ml
@@ -10,3 +10,21 @@ type portfolio = {
   bank_account : int;
   followed_stocks : stock list;
 }
+
+(** Create a portfolio with [initial_balance] and [initial_bank_account]*)
+let create_portfolio initial_balance initial_bank_account =
+  failwith "Unimplemented"
+
+(** Add [stock] to the watchlist of the portfolio*)
+let add_stock portfolio stock = failwith "Unimplemented"
+
+(** Update balance by [amount]. Print out a message to indicate the updated
+    balance. If [amount] is negative and its absolute value exceeds current
+    balance, produce a message "Out of balance: [balance + amount] needed"*)
+let update_balance portfolio amount = failwith "Unimplemented"
+
+(** Update the current bank account. *)
+let update_bant_account portfolio x = failwith "Unimplemented"
+
+(** Remove a stock from the watchlist. Required: the stock is in the watchlist. *)
+let remove_stock portfolio symbol = failwith "Unimplemented"
--- test/main.ml
+++ test/main.ml
@@ -1,6 +1,11 @@
 (*If giving errors, remember to run [dune build]*)
 
 open OUnit2
+open Stocks
+open Portfolio
+open Parser
+open Scraper
+open Stock
 
 let tests = "stocks test suite" >::: []
 let _ = run_test_tt_main tests
