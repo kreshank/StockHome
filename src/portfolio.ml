4a39bb1393afed73f1aa9f254a6b9fef85e7bbd4
liubodong <bl576@cornell.edu>
2023-10-20 15:48:30 -0400

    Implement type and all functions in src/portfolio ml 10/20/2023 - bl576@cornell.edu
    - Made a module tupe PortfolioType
    - Made a module Portfolio of type PortfolioType
    - Moved all functions into Portfolio module
    - Added a of_string function

--- src/portfolio.ml
+++ src/portfolio.ml
@@ -1,30 +1,85 @@
 (* Portfolio.ml - Intended to store portfolio data *)
+
+open Stock
+
 type stock = {
   symbol : string;
   price : float;
   quantity : int;
 }
 
-type portfolio = {
-  balance : float;
-  bank_account : int;
-  followed_stocks : stock list;
-}
+module type PortfolioType = sig
+  type t
 
-(** Create a portfolio with [initial_balance] and [initial_bank_account]*)
-let create_portfolio initial_balance initial_bank_account =
-  failwith "Unimplemented"
+  val of_string : t -> string
+  (* Returns a human-readable string of information of a portfolio*)
 
-(** Add [stock] to the watchlist of the portfolio*)
-let add_stock portfolio stock = failwith "Unimplemented"
+  val create_portfolio : float -> int -> t
+  (** Create a portfolio with [initial_balance] and [initial_bank_account]*)
 
-(** Update balance by [amount]. Print out a message to indicate the updated
-    balance. If [amount] is negative and its absolute value exceeds current
-    balance, produce a message "Out of balance: [balance + amount] needed"*)
-let update_balance portfolio amount = failwith "Unimplemented"
+  val add_stock : t -> stock -> t
+  (** Add [stock] to the watchlist of the portfolio*)
 
-(** Update the current bank account. *)
-let update_bant_account portfolio x = failwith "Unimplemented"
+  val update_balance : t -> float -> t
+  (** Update balance by [amount]. Print out a message to indicate the updated
+      balance. If [amount] is negative and its absolute value exceeds current
+      balance, produce a message "Out of balance: [balance + amount] needed"*)
 
-(** Remove a stock from the watchlist. Required: the stock is in the watchlist. *)
-let remove_stock portfolio symbol = failwith "Unimplemented"
+  val update_bant_account : t -> int -> t
+  (** Update the current bank account. *)
+
+  val remove_stock : t -> stock -> t
+  (** Remove a stock from the watchlist. Required: the stock is in the
+      watchlist. *)
+end
+
+module Portfolio : PortfolioType = struct
+  type t = {
+    balance : float;
+    bank_account : int;
+    followed_stocks : stock list;
+  }
+
+  (* Returns a human-readable string of information of a portfolio*)
+  let of_string portfolio =
+    "Balance: "
+    ^ string_of_float portfolio.balance
+    ^ ". Bank account: "
+    ^ string_of_int portfolio.bank_account
+
+  (** Create a portfolio with [initial_balance] and [initial_bank_account]*)
+  let create_portfolio initial_balance initial_bank_account =
+    {
+      balance = initial_balance;
+      bank_account = initial_bank_account;
+      followed_stocks = [];
+    }
+
+  (** Add [stock] to the watchlist of the portfolio*)
+  let add_stock portfolio stock =
+    { portfolio with followed_stocks = stock :: portfolio.followed_stocks }
+
+  (** Update balance by [amount]. Print out a message to indicate the updated
+      balance. If [amount] is negative and its absolute value exceeds current
+      balance, produce a message "Out of balance: [balance + amount] needed"*)
+  let update_balance portfolio amount =
+    if portfolio.balance +. amount >= 0.0 then (
+      print_string
+        ("Balance updated: " ^ string_of_float (portfolio.balance +. amount));
+      { portfolio with balance = portfolio.balance +. amount })
+    else (
+      print_string
+        ("Still need: " ^ string_of_float (0.0 -. portfolio.balance -. amount));
+      portfolio)
+
+  (** Update the current bank account. *)
+  let update_bant_account portfolio x = { portfolio with bank_account = x }
+
+  (** Remove a stock from the watchlist. Required: the stock is in the
+      watchlist. *)
+  let remove_stock portfolio stock =
+    let updated_stocks =
+      List.filter (fun x -> x <> stock) portfolio.followed_stocks
+    in
+    { portfolio with followed_stocks = updated_stocks }
+end
--- src/portfolio.mli
+++ src/portfolio.mli
@@ -2,11 +2,8 @@
 
 (* Message to who's making stock.ml: This should be from the stock module but
    I'll just leave it here since stock is not implemented yet*)
-type stock = {
-  symbol : string;
-  price : float;
-  quantity : int;
-}
+
+open Stock
 
 type portfolio = {
   balance : float;
@@ -14,19 +11,39 @@ type portfolio = {
   followed_stocks : stock list;
 }
 
-val create_portfolio : float -> int -> portfolio
-(** Create a portfolio with [initial_balance] and [initial_bank_account]*)
+type stock = {
+  symbol : string;
+  price : float;
+  quantity : int;
+}
 
-val add_stock : portfolio -> stock -> portfolio
-(** Add [stock] to the watchlist of the portfolio*)
+module type PortfolioType = sig
+  type t
 
-val update_balance : portfolio -> float -> portfolio
-(** Update balance by [amount]. Print out a message to indicate the updated
-    balance. If [amount] is negative and its absolute value exceeds current
-    balance, produce a message "Out of balance: [balance + amount] needed"*)
+  val of_string : t -> string
+  (* Returns a human-readable string of information of a portfolio*)
 
-val update_bant_account : portfolio -> int -> portfolio
-(** Update the current bank account. *)
+  val create_portfolio : float -> int -> t
+  (** Create a portfolio with [initial_balance] and [initial_bank_account]*)
 
-val remove_stock : portfolio -> string -> portfolio
-(** Remove a stock from the watchlist. Required: the stock is in the watchlist. *)
+  val add_stock : t -> string -> t
+  (** Add [stock] to the watchlist of the portfolio*)
+
+  val update_balance : t -> float -> t
+  (** Update balance by [amount]. Print out a message to indicate the updated
+      balance. If [amount] is negative and its absolute value exceeds current
+      balance, produce a message "Out of balance: [balance + amount] needed"*)
+
+  val update_bant_account : t -> int -> t
+  (** Update the current bank account. *)
+
+  val remove_stock : t -> string -> t
+  (** Remove a stock from the watchlist. Required: the stock is in the
+      watchlist. *)
+end
+
+type portfolio = {
+  balance : float;
+  bank_account : int;
+  followed_stocks : stock list;
+}
