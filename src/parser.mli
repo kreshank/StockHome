549acb76468a469b1571054b758d352cad5274b9
Leo Lu <ll865@cornell.edu>
2023-10-20 16:58:58 -0400

    Updated to_stock and slice_list
    10/20/23 - ll865@cornell.edu
    - Updated to_stock and slice_list to return options instead of failing
      when ticker is not present in parser

--- src/parser.mli
+++ src/parser.mli
@@ -14,13 +14,14 @@ module type ParserType = sig
      Reads the csv file given and returns an updated map based on the
      information present in the csv.*)
 
-  val to_stock : string -> t -> Stock.t
+  val to_stock : string -> t -> Stock.t option
   (** Given a ticker, returns a Stock representing the current information of
-      the stock present in the parser.*)
+      the stock present in the parser. Returns [None] if ticker is not in
+      Parser.*)
 
-  val slice_list : string -> t -> slice list
-  (** Given a ticker, return the slice list associated with the ticker.
-      Failswith [Ticker Not Found] if ticker is not in Parser *)
+  val slice_list : string -> t -> slice list option
+  (** Given a ticker, return the slice list associated with the ticker. Returns
+      [None] if ticker is not in Parser. *)
 end
 
 module Parser : ParserType
