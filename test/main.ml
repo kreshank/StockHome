6d09acd3cf5b84ca1e7d4b142f36c40d5144d4f9
Leo Lu <ll865@cornell.edu>
2023-10-20 10:16:31 -0400

    Updated info files, Added test cases for parser
    10/20/23 - ll865@cornell.edu
    -Changed name of stock_info and added simplified stock_info to data folder for testing
    -Added test case to test suite

--- test/main.ml
+++ test/main.ml
@@ -22,3 +22,20 @@ let tests =
        ]
 
 let _ = run_test_tt_main tests
+
+(* PARSER TESTS *)
+let s1 =
+  ",open,high,low,close,adjclose,volume,ticker\n\
+   2023-10-02,110.9,111.8,109.7,110.9,110.9,1569000,A"
+
+let simple_map = Parser.of_csv "data/stock_info_simple.csv"
+
+let tests =
+  "parser.ml Test Suite"
+  >::: [
+         ( "Simple Parse" >:: fun _ ->
+           print_string
+             (Stock.of_string_detailed (Parser.to_stock "A" simple_map)) );
+       ]
+
+let _ = run_test_tt_main tests
