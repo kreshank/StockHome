3d33d4a0f7e156067bd8d7c86be5c3021e0605e5
Bhuwan Bhattarai <bb623@cornell.edu>
2023-11-27 23:54:25 -0500

    Create starter GUI code
    11/27/23 - bb623@cornell.edu
    - got bogue working

---
+++ src/gui.ml
@@ -0,0 +1,30 @@
+open Bogue
+module W = Widget
+module L = Layout
+
+let main () =
+  (* create label widgets for heading *)
+  let title_label = W.label "OCAML STOCKS" in
+  let date_label = W.label "November 21, 2023" in
+  let watch_lst_label = W.label "Watch List" in
+  let portfolio_lst_label = W.label "My Portfolio" in
+
+  (* create main containers *)
+  let heading_container =
+    L.flat_of_w ~name:"heading_container" [ title_label; date_label ]
+  in
+  let second_tier_container =
+    L.flat_of_w ~name:"second_tier_container"
+      [ watch_lst_label; portfolio_lst_label ]
+  in
+
+  let main_container =
+    L.tower ~name:"main_container" [ heading_container; second_tier_container ]
+  in
+
+  let board = Bogue.of_layout main_container in
+  Bogue.run board
+
+let () =
+  main ();
+  Bogue.quit ()
