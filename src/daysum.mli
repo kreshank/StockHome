4e2dabc989ef21de694d382908ea0af9c5f98693
Bhuwan Bhattarai <bb623@cornell.edu>
2023-12-09 03:02:01 -0500

    Remove ticker on add - Ticker is now removed when add button is pressed
    - bb623@cornell.edu

--- src/daysum.mli
+++ src/daysum.mli
@@ -21,45 +21,45 @@ module type DaySumType = sig
   val timestamp : t -> date * time
   (** [timestamp ds] returns the timestamp and retrieval time of [ds] data. *)
 
-  val targ_est_1y : t -> float
+  val targ_est_1y : t -> float option
   (** [targ_est_1y ds] returns Yahoo Finance's 1 year price estimate of the
       stock associated with [ds]. *)
 
-  val week_range_52w : t -> float * float
+  val week_range_52w : t -> float option * float option
   (** [week_range_52w ds] returns price range [(min, max)] over last 52 weeks of
       the stock associated with [ds]. *)
 
-  val ask : t -> float * int
+  val ask : t -> float option * int option
   (** [ask ds] returns the ask [(price, quantity)] where [price] is the lowest a
       stockholder is willing to sell this stock at a particular [quantity]. *)
 
-  val avg_day_vol : t -> int
+  val avg_day_vol : t -> int option
   (** [avg_day_vol] returns the average day volume of the stock associated with
       [ds]. *)
 
-  val beta_5y_mly : t -> float
+  val beta_5y_mly : t -> float option
   (** [beta_5y_mly ds] returns the beta (5y monthly) of the stock associated
       with [ds]. *)
 
-  val bid : t -> float * int
+  val bid : t -> float option * int option
   (** [bid ds] returns the bid [(price, quantity)] where [price] is the highest
       a stockholder is willing to buy this stock at a particular [quantity]. *)
 
-  val day_range : t -> float * float
+  val day_range : t -> float option * float option
   (** [day_range ds] returns the price range of this stock, over today
       [(min, max)]. *)
 
-  val eps_ttm : t -> float
+  val eps_ttm : t -> float option
   (** [eps_ttm ds] returns the Trailing Twelve Month earnings per share for this
       stock. *)
 
-  val earnings_date : t -> date * date
+  val earnings_date : t -> date option * date option
   (** [earning_date] returns the earnings date [(start, end)] of this stock. *)
 
-  val ex_date : t -> date
+  val ex_date : t -> date option
   (** [ex_date ds] returns the ex-dividend date for the stock of this stock. *)
 
-  val forward_div_yield : t -> float * float
+  val forward_div_yield : t -> float option * float option
   (** [forward_div_yield] returns the forward dividend and yield percentage in
       format [(dividend_price, yield)], where yield is in percentage. *)
 
@@ -69,7 +69,7 @@ module type DaySumType = sig
   val open_price : t -> float
   (** [open_price ds] returns the open_price of this stock at time of retrieval. *)
 
-  val pe_ratio_ttm : t -> float
+  val pe_ratio_ttm : t -> float option
   (** [pe_ratio_ttm ds] returns the trailing twelve month PE ratio of the stock. *)
 
   val prev_close : t -> float
